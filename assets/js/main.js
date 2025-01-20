42271fe939a702902190129ccc1110fa401446b8
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2025-01-21 02:40:19 +0600

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -947,44 +947,48 @@
 
 
   // typewritter text 
-  typing(0, $('.typewriter-text').data('text'));
+  if (document.querySelectorAll(".typewriter-text").length > 0) {
 
-  function typing(index, text) {
+    typing(0, $('.typewriter-text').data('text'));
 
-    var textIndex = 1;
+    function typing(index, text) {
 
-    var tmp = setInterval(function () {
-      if (textIndex < text[index].length + 1) {
-        $('.typewriter-text').text(text[index].substr(0, textIndex));
-        textIndex++;
-      } else {
-        setTimeout(function () { deleting(index, text) }, 2000);
-        clearInterval(tmp);
-      }
+      var textIndex = 1;
 
-    }, 150);
+      var tmp = setInterval(function () {
+        if (textIndex < text[index].length + 1) {
+          $('.typewriter-text').text(text[index].substr(0, textIndex));
+          textIndex++;
+        } else {
+          setTimeout(function () { deleting(index, text) }, 2000);
+          clearInterval(tmp);
+        }
 
+      }, 150);
+
+    }
+
+    function deleting(index, text) {
+
+      var textIndex = text[index].length;
+
+      var tmp = setInterval(function () {
+
+        if (textIndex + 1 > 0) {
+          $('.typewriter-text').text(text[index].substr(0, textIndex));
+          textIndex--;
+        } else {
+          index++;
+          if (index == text.length) { index = 0; }
+          typing(index, text);
+          clearInterval(tmp);
+        }
+
+      }, 150)
+
+    }
   }
 
-  function deleting(index, text) {
-
-    var textIndex = text[index].length;
-
-    var tmp = setInterval(function () {
-
-      if (textIndex + 1 > 0) {
-        $('.typewriter-text').text(text[index].substr(0, textIndex));
-        textIndex--;
-      } else {
-        index++;
-        if (index == text.length) { index = 0; }
-        typing(index, text);
-        clearInterval(tmp);
-      }
-
-    }, 150)
-
-  }
 
 
 
